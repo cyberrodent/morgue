de8a89dd97968f0a86c7de8511e80dd2f6780368
CyberRodent <fej.reblok@gmail.com>
2015-02-14 01:07:07 -0500

    this gets the errors to stop but stuff is still messed up

--- views/page.php
+++ views/page.php
@@ -14,6 +14,7 @@
 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="/assets/js/jquery.plugin.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js"></script>
   </head>
   <body>
